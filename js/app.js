6148d1b7f1eb310f759f161db98e71722b71c284
hermelen <Hermelen>
2018-07-12 15:40:35 +0200

    shape worksgit add .git add .git add .

--- js/app.js
+++ js/app.js
@@ -1,20 +1,38 @@
 var mymap = L.map('mapid').setView([47.2250638, 2.690954], 6);
-    // ,6z
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+// ,6z
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(mymap);
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    setInterval(function(){
-        var height = $(window).height();
-        $("#mapid").height(height);
-    }, 1000);
+	$.ajax({
+		url: 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=contours-simplifies-des-departements-francais-2015&facet=code_dept',
+		// type: 'GET',
+		datatype: 'json',
+		success: function(data, statut) {
+			displayCities(data) 
+		},
+		error: function(resultat, statut, erreur) {
 
-  console.log('hello world');
+		},
+		complete: function(resultat, statut) {
+
+		}
+	})
+
+	function displayCities(data) {
+		for (dept of data['records']) {
+			var shape = dept['fields']['geo_shape'];
+			L.geoJSON(shape).addTo(mymap);
+		}
+	}
+
+	// var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
 })
