9949f1681d4dbae5fac40fc2c3b2a57bf82546de
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-07-12 14:23:18 +0200

    map is visible, gps was in france

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+var mymap = L.map('mapid').setView([47.2250638, 2.690954], 6);
+    // ,6z
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+
 $(document).ready(function(){
+
+    setInterval(function(){
+        var height = $(window).height();
+        $("#mapid").height(height);
+    }, 1000);
+
   console.log('hello world');
 })
