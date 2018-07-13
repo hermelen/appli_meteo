2889453426192e75a9ecb63160f5d65e45f190bf
hermelen <Hermelen>
2018-07-13 10:37:59 +0200

    icon marker

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
-
-
 $(document).ready(function() {
 	var mymap = L.map('mapid').setView([47.2250638, 2.690954], 6);
 	// ,6z
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
 
@@ -17,7 +15,6 @@ $(document).ready(function() {
 		datatype: 'json',
 		success: function(data, statut) {
 			displayCities(data);
-            // weather(displayCities(data));
 		},
 		error: function(resultat, statut, erreur) {
 
@@ -30,26 +27,35 @@ $(document).ready(function() {
 	function displayCities(data) {
 		for (dept of data['records']) {
 			var shape = dept['fields']['geo_shape'];
-            var name = dept['fields']['nom_chf'];
-            var geoPoint = dept['fields']['geo_point_2d'];
-            L.geoJSON(shape).addTo(mymap);
-			// var marker = L.marker([dept['fields']['geo_point_2d'][0], dept['fields']['geo_point_2d'][1]]).addTo(mymap);
-			// function weather(name) {
-			console.log("api.openweathermap.org/data/2.5/weather?q="+name+",fr&APPID=9320a50df4522e7606195b5c99b563a6");
-			    $.ajax({
-			        url: "http://api.openweathermap.org/data/2.5/weather?q="+name+",fr&APPID=9320a50df4522e7606195b5c99b563a6",
-			        datatype: 'json',
-			        success: function(data, statut) {
-						console.log("success");
-					    var marker = L.marker([data['coord']["lat"],data['coord']["lon"]]).addTo(mymap);
-					},
-					error: function(resultat, statut, erreur) {
-						console.log("error");
-					},
-					complete: function(resultat, statut) {
-						console.log("complete");
-					}
-			    })
+			var name = dept['fields']['nom_chf'];
+			var geoPoint = dept['fields']['geo_point_2d'];
+			L.geoJSON(shape).addTo(mymap);
+			$.ajax({
+				url: "http://api.openweathermap.org/data/2.5/weather?q=" + name + ",fr&APPID=9320a50df4522e7606195b5c99b563a6",
+				datatype: 'json',
+				success: function(data, statut) {
+					// +data['weather'][0]['description']
+					var img = "<img src='https://openweathermap.org/img/w/" + data['weather'][0]['icon'] + ".png' alt=''>";
+					var imgUrl = "https://openweathermap.org/img/w/" + data['weather'][0]['icon'] + ".png";
+					var thisIcon = L.icon({
+						iconUrl: imgUrl,
+						// shadowUrl: 'leaf-shadow.png',
+
+						iconSize: [38, 38], // size of the icon
+						// shadowSize: [50, 64], // size of the shadow
+						// iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+						// shadowAnchor: [4, 62], // the same for the shadow
+						// popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+					});
+					var popUpData = data['name'] + ", " + img;
+					L.marker([data['coord']["lat"], data['coord']["lon"]], {
+						icon: thisIcon
+					}).bindPopup(popUpData).openPopup().addTo(mymap);
+
+				},
+				error: function(resultat, statut, erreur) {},
+				complete: function(resultat, statut) {}
+			})
 			// }
 		}
 	}
