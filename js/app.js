8bf98b678626ee1da53c31c3e25a53c102926fe6
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-07-13 11:57:50 +0200

    temp

--- js/app.js
+++ js/app.js
@@ -43,16 +43,18 @@ $(document).ready(function() {
 
 						iconSize: [38, 38], // size of the icon
 						// shadowSize: [50, 64], // size of the shadow
-						// iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+						iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
 						// shadowAnchor: [4, 62], // the same for the shadow
 						// popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 					});
-					var popUpData = data['name'] + ", " + img;
+					var cel = Math.round(data['main']["temp"] - 273.15);
+					var popUpData = data['name'] + ", " + img + cel;
 					L.marker([data['coord']["lat"], data['coord']["lon"]], {
 						icon: thisIcon
 					}).bindPopup(popUpData).openPopup().addTo(mymap);
 
 				},
+
 				error: function(resultat, statut, erreur) {},
 				complete: function(resultat, statut) {}
 			})
